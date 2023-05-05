891e21a4dc4828804801aeab4191a2bde4e9d766
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-05-04 23:24:16 -0600

    Reorganize existing code for modularity
    Organized code into smaller, clearer modules.
    Updated namespaces accordingly.

--- app.js
+++ app.js
@@ -1,104 +1,82 @@
-let gameBoard = {
+const createGamePieces = (() => {
+  let gameBoard = {
     firstRow: ["a", "", ""],
-    secondRow: ["", "b", ""],
+    secondRow: ["", "b", "f"],
     thirdRow: ["", "c", ""]
-};
+  };
 
-//prompt()s twice for player name.
-//assigns a token based on whether it's the first or second name.
-//pushes new Player objects to the playerArray 
-//returns the array, accessible as createPlayerArray.makePlayer() outside the scope
-const createPlayerArray = (() => {
-    let playerArray = [];
+  const getBoard = () => gameBoard;
 
-    function Player (name, token) {
-        return {
-            name,
-            token
+  let playerArray = [];
+
+  function Player (name, token) {
+      return {
+          name,
+          token
+      }
+  }
+
+  const makePlayers = () => {
+    for (let i = 0; i < 2; i++) {
+
+      function promptForName () {
+        return prompt('player name: ');
+      }
+
+      function determineToken () {
+        if (i === 0) {
+          return 'X';
+        } else if (i === 1) {
+          return 'O';
         }
+      }
+        
+      playerArray.push(Player(promptForName(), determineToken()));
+        
     }
+    return playerArray;
+  };
 
-    const makePlayer = () => {
-        for (let i = 0; i < 2; i++) {
-
-            function promptForName () {
-                return prompt('player name: ');
-            }
-    
-            function determineToken () {
-                if (i === 0) {
-                    return 'X';
-                } else if (i === 1) {
-                    return 'O';
-                }
-            }
-            
-            playerArray.push(Player(promptForName(), determineToken()));
-            
-        }
-        return playerArray;
-    };
-  
-    return { makePlayer };
+  return { 
+    makePlayers,
+    getBoard 
+  };
 })();
+
 
 //should: place token, check win conditions, switch player
 const turnController = (() => {
-    const playerArray = createPlayerArray.makePlayer();
-    let activePlayer = playerArray[0]; //can put .name or .token after the index
-    let activeToken = activePlayer.token;
-    const boardSpace = document.querySelector('.game-board');
+  const playerArray = createGamePieces.makePlayers();
+  let activePlayer = playerArray[0];
 
-    //loop through the array values in gameBoard and
-    //print them as buttons in the DOM
-    //should probably be executed right away
-    //for some reason it is not working. it works in codepen more or less
-    //but is less nested there.
-    const drawBoard = (obj, space) => {
-      for (const arr in obj) {
-        obj[arr].forEach((cell, index) => {
-          const slotBtn = document.createElement('button');
-          slotBtn.classList.add('slot');
-          slotBtn.textContent = cell;
-          slotBtn.setAttribute('id', index);
-          space.appendChild(slotBtn);
-        });
+  const switchPlayerTurn = () => {
+
+      if (activePlayer === playerArray[0]) {
+        activePlayer = playerArray[1];
+      } else {
+        activePlayer = playerArray[0];
       }
-    };
-    drawBoard(gameBoard, boardSpace);
+      console.log(activePlayer);
+      return activePlayer;
+  }
 
-    const switchPlayerTurn = () => {
-        if (activePlayer === playerArray[0]) {
-            activePlayer = playerArray[1];
-        } else {
-            activePlayer = playerArray[0];
-        }
-        return activePlayer;
-    }
-    
-    return { 
-        switchPlayerTurn,
-        activePlayer
-    };
-})();
+  const getActive = () => activePlayer;
 
-//pass in:
-//object.firstRow etc.,
-//0-indexed index
-//and player token (X or O)
-function placeToken (row, index, token) {
-    if (row[index] === '') {
-      row.splice(index, 1, token);
+  //updates the gameBoard object arrays
+  const placeToken = (row, index, token) => {
+    if (createGamePieces.getBoard()[row][index] === '') {
+      createGamePieces.getBoard()[row].splice(index, 1, token);
+      switchPlayerTurn();
     } else {
         alert('please choose another spot.');
     }
-}
+  }
 
-//win conditions
-//horizontal, vertical and diagonal
-//ugliest code ever. i hate it
-//I can use "every" for this...
-const checkForWinner = ((board) => {
+  //win conditions
+  //horizontal, vertical and diagonal
+  //ugliest code ever. i hate it
+  //I can use "every" for this...
+  const checkForWinner = (board) => {
     for (const row in board) {
       if (board[row][0] !== '') {
         if (board[row][0] === board[row][1] && board[row][0] === board[row][2]) {
@@ -114,13 +92,61 @@ const checkForWinner = ((board) => {
     }
     }
   }
-  
+
   if (board.firstRow[0] === board.secondRow[1] && board.firstRow[0] === board.thirdRow[2] && board.firstRow[0] !== '') {
     console.log(`winner! ${board.firstRow[0]}`);
   }
-  
+
   if (board.firstRow[2] === board.secondRow[1] && board.firstRow[2] === board.thirdRow[0] && board.firstRow[2] !== '') {
     console.log(`winner! ${board.firstRow[2]}`);
   }
+
+  };
   
-})(gameBoard);
+  return { 
+      placeToken,
+      checkForWinner,
+      getActive
+  };
+})();
+
+
+const viewController = (() => {
+  const boardSpace = document.querySelector('.game-board');
+
+  //loop through the array values in gameBoard and
+  //print them as buttons in the DOM
+  const drawBoard = (obj, space) => {
+    boardSpace.textContent = '';
+    for (const arr in obj) {
+      obj[arr].forEach((cell, index) => {
+        console.log(cell);
+        const slotBtn = document.createElement('button');
+        slotBtn.classList.add('slot');
+        slotBtn.textContent = cell;
+        slotBtn.setAttribute('data-row', arr);
+        slotBtn.setAttribute('id', index);
+        space.appendChild(slotBtn);
+      });
+    }
+  };
+  drawBoard(createGamePieces.getBoard(), boardSpace); //i think this has to be here. somehow
+
+
+  const playOnClick = (() => {
+    const slot = document.querySelectorAll('.slot');
+    slot.forEach(item => {
+      item.addEventListener('click', () => {
+        let itemRow = item.getAttribute('data-row');
+        turnController.placeToken(itemRow, item.id, turnController.getActive().token);
+        console.table(createGamePieces.getBoard());
+        //console.log(turnController.activePlayer.token);
+        //drawBoard(createGamePieces.getBoard(), boardSpace); //with this here, we can only click once
+        turnController.checkForWinner(createGamePieces.getBoard());
+        })
+      })
+  })();
+
+
+})();
+
