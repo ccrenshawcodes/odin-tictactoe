0252e11541e74f5728b94c1dbe0edec90c56f386
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-05-25 21:15:24 -0600

    Organize code into the module pattern
    Added a function to create the player array as well

--- app.js
+++ app.js
@@ -1,38 +1,104 @@
-const gameBoard = ['x','','','x','','','','o',''];
+const controlBoard = (() => {
 
-const playerList = [];
+  const gameBoard = ['x','','','x','','','','o',''];
+  const getBoard = () => gameBoard;
 
-function Player (playerName, token) {
+  function placeToken (board, position, player) {
+    if (board[position] === '') {
+      board[position] = player;
+      document.querySelector('.winner-message').textContent = '';
+  
+    } else {
+      document.querySelector('.winner-message').textContent = 'this spot is already taken.';
+    }
+  }
+
   return {
-    playerName,
-    token
+    getBoard,
+    placeToken,
   }
-};
 
-function renderBoard (board) {
+})();
+
+const controlGameFlow = (() => {
+
+  //  creating the list of players
+  const playerList = [];
+
+  function Player (playerName, token) {
+    return {
+      playerName,
+      token
+    }
+  };
+
+  function determineToken (arr) {
+    if (arr.length <= 0) {
+      return 'X';
+    } else if (arr.length >= 1) {
+      return 'O';
+    }
+  }
+
+  const addNewPlayer = (() => {
+    const addPlayerOne = document.querySelector('#add-player-one');
+    const addPlayerTwo = document.querySelector('#add-player-two');
+    let playerOneNameField = document.querySelector('#player-one');
+    let playerTwoNameField = document.querySelector('#player-two');
+
+    addPlayerOne.addEventListener('click', () => {
+      playerList.push(Player(playerOneNameField.value, determineToken(playerList)));
+    })
+
+    addPlayerTwo.addEventListener('click', () => {
+      playerList.push(Player(playerTwoNameField.value, determineToken(playerList)));
+    })
+
+  })();
+
+
+  function checkForWinner (board) {
+    //  code will go here
+    //  it will be ugly. Guaranteed
+  }
+
+  return {
+    playerList,
+    checkForWinner,
+  }
+
+})();
+
+const controlDisplay = (() => {
+
+  function renderBoard (board) {
+    const slots = document.querySelectorAll('.slot');
+    let counter = 0;
+  
+    board.forEach(item => {
+      slots[counter].textContent = item;
+      counter++;
+    })
+  }
+  renderBoard(controlBoard.getBoard());
+
+
+  //  open and close the modal
+  const openModal = document.querySelector('.start-game');
+  openModal.addEventListener('click', () => {
+    document.querySelector('.start-modal').style.display = 'block';
+  })
+  const closeBtn = document.querySelector('.exit');
+  closeBtn.addEventListener('click', () => {
+    document.querySelector('.start-modal').style.display = 'none';
+  })
+  
+  //  adds event listeners to the slots to allow us to place tokens
   const slots = document.querySelectorAll('.slot');
-  let counter = 0;
-
-  board.forEach(item => {
-    slots[counter].textContent = item;
-    counter++;
+  slots.forEach(slot => {
+    slot.addEventListener('click', () => {
+      controlBoard.placeToken(controlBoard.getBoard(), slot.getAttribute('data-position'), 'C'); //need to change the 'C' to something meaningful
+      renderBoard(controlBoard.getBoard());
+    })
   })
-}
-
-renderBoard(gameBoard);
-
-function placeToken (board, position, player) {
-  if (board[position] === '') {
-    board[position] = player;
-  } else {
-    document.querySelector('.winner-message').textContent = 'this spot is already taken.';
-  }
-  renderBoard(board);
-}
-
-const slots = document.querySelectorAll('.slot');
-slots.forEach(slot => {
-  slot.addEventListener('click', () => {
-    placeToken(gameBoard, slot.getAttribute('data-position'), 'C');
-  })
-})
+})();
